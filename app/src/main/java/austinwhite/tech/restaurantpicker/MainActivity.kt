a3e5930844954a32a166e309e539156d27f776e9
endlessrequiem <austin.j.white.96@gmail.com>
2020-07-05 20:57:58 -0700

    improved main layout

--- app/src/main/java/austinwhite/tech/restaurantpicker/MainActivity.kt
+++ app/src/main/java/austinwhite/tech/restaurantpicker/MainActivity.kt
@@ -1,12 +1,14 @@
 package austinwhite.tech.restaurantpicker
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.util.ArrayList
-import android.util.Log
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val input: EditText = findViewById(R.id.userInput)
+        val removeInput: EditText = findViewById(R.id.removeInput)
         val add: Button = findViewById(R.id.add)
         val clear: Button = findViewById(R.id.clear)
         val choose: Button = findViewById(R.id.choose)
+        val removeButton: Button = findViewById(R.id.remove)
         val choices: TextView = findViewById(R.id.choices)
         val result: TextView = findViewById(R.id.result)
 
@@ -25,7 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         add.setOnClickListener {
             if (input.text.toString().isBlank()){
-                result.text = getString(R.string.noInput)
+                Toast.makeText(this@MainActivity, getString(R.string.empty), Toast.LENGTH_SHORT).show()
+
             } else {
                 choicesArr.add(input.text.toString())
                 choices.text = choicesArr.toString().replace("[", "").replace("]", "")
@@ -46,6 +51,27 @@ class MainActivity : AppCompatActivity() {
                 result.text = getString(R.string.empty)
             } else {
                 result.text = choicesArr.random().toString()
+            }
+        }
+
+        removeButton.setOnClickListener {
+            val toRemove = removeInput.text.toString()
+
+            if (toRemove.isBlank()) {
+            Toast.makeText(this@MainActivity, "Nothing Here!", Toast.LENGTH_SHORT).show()
+
+        } else {
+                if (choicesArr.contains(toRemove)) {
+                    choicesArr.remove(toRemove)
+                    choices.text = choicesArr.toString().replace("[", "").replace("]", "")
+                    Log.i("Debug Check", choicesArr.toString()) //for debug purposes
+                    removeInput.setText("")
+
+                } else {
+                    Toast.makeText(this@MainActivity, "Not in Array", Toast.LENGTH_SHORT).show()
+
+
+                }
             }
         }
 
